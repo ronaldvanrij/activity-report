e875bd54a67ac549d955af4a03ef6a980aceab5a
Ronald van Rij <ronaldvanrij@users.noreply.github.com>
2023-11-28 16:02:19 +0100

    Allow to create the report anytime, but use cron to determine timespan

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,15 @@ import { Issues } from './issues'
 import { Renderer } from './render'
 
 async function run() {
-  if (context.eventName === 'schedule') {
-    const config = Util.getInputs()
-    const timespan = Util.getTimespan()
+  const config = Util.getInputs()
+  const timespan = Util.getTimespan()
 
-    core.info(`Report from ${timespan.fromDate} to ${timespan.toDate}`)
+  core.info(`Report from ${timespan.fromDate} to ${timespan.toDate}`)
 
-    const title = Renderer.renderTitle(timespan, config)
-    const body = await Renderer.renderBody(timespan, config)
-    const labels = config.addLabels || `${Util.lcfirst(timespan.name)}-report`
-    await Issues.create(title, body, labels)
-  }
+  const title = Renderer.renderTitle(timespan, config)
+  const body = await Renderer.renderBody(timespan, config)
+  const labels = config.addLabels || `${Util.lcfirst(timespan.name)}-report`
+  await Issues.create(title, body, labels)
 }
 
 run()
